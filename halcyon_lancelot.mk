26db7b4fe64a26d23c61bf1bfc06a3433b01fcf4
HeroBuxx <me@herobuxx.me>
2024-10-31 11:49:28 +0000

    lancelot: Enchanting lancelot
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- halcyon_lancelot.mk
+++ halcyon_lancelot.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
-PRODUCT_NAME := lineage_lancelot
+PRODUCT_NAME := halcyon_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
